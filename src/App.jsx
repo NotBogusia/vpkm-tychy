ae17c0ae3e97a6ed4692ef657e4a0d2d81e5ebc6
NotBogusia <michalo294rt@gmail.com>
2026-06-24 21:25:37 +0200

    crew fix v2

--- src/App.jsx
+++ src/App.jsx
@@ -834,7 +834,7 @@ const handleSaveDriver = async (e) => {
     );
   }
 
-  return 
+  return (
     <div className="min-h-screen bg-zinc-950 text-zinc-100 font-sans flex justify-center p-4 sm:p-8">
       <div className="w-full max-w-4xl flex flex-col gap-6">
 
@@ -1136,4 +1136,4 @@ const handleSaveDriver = async (e) => {
         </main>
       </div>
     </div>
-  
+  );
